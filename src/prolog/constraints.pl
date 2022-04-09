896f859a1a23f0eb49bcfcf78b256ceab169cdc8
Clara Martins <50105043+LeKinaSa@users.noreply.github.com>
2022-04-09 18:13:22 +0100

    Fixes on Constraint : 1 Task per Machine

--- src/prolog/constraints.pl
+++ src/prolog/constraints.pl
@@ -22,7 +22,7 @@ only_one_task_per_machine_at_a_time([_ | Tasks], [_ | Starts], [_ | Ends], [YesO
     only_one_task_per_machine_at_a_time(Tasks, Starts, Ends, Chosen).
 only_one_task_per_machine_at_a_time([_-(Machine-_) | Tasks], [Start | Starts], [End | Ends], [YesOrNo | Chosen]) :-
     YesOrNo #= 1, % Task was Chosen, so no other task can occur in the same machine at the same time
-    no_other_task(Machine, Start-End, Starts, Ends, Chosen),
+    no_other_task(Machine, Start-End, Tasks, Starts, Ends, Chosen),
     only_one_task_per_machine_at_a_time(Tasks, Starts, Ends, Chosen).
 
 % no_other_task(+Machine, +Start-End, +Tasks, +Starts, +Ends, +Chosen)
@@ -36,12 +36,12 @@ no_other_task(TaskMachine, Interval, [_-(Machine-_) | Tasks], [_ | Starts], [_ |
 no_other_task(TaskMachine, StartTask-EndTask, [_-(Machine-_) | Tasks], [_ | Starts], [End | Ends], [YesOrNo | Chosen]) :-
     YesOrNo #= 1,
     TaskMachine #= Machine,
-    End =< StartTask, % The other Task was before this one
+    End #=< StartTask, % The other Task was before this one
     no_other_task(TaskMachine, StartTask-EndTask, Tasks, Starts, Ends, Chosen).
 no_other_task(TaskMachine, StartTask-EndTask, [_-(Machine-_) | Tasks], [Start | Starts], [_ | Ends], [YesOrNo | Chosen]) :-
     YesOrNo #= 1,
     TaskMachine #= Machine,
-    EndTask =< Start, % The other Task was after this one
+    EndTask #=< Start, % The other Task was after this one
     no_other_task(TaskMachine, StartTask-EndTask, Tasks, Starts, Ends, Chosen).
 
 %%%%%%%%%%%%%%%%%%%% 1 Alternative Task %%%%%%%%%%%%%%%%%%%%
