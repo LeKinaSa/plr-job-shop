09d8c4fd2cce4873a679a33e3199f7e430abf11e
Clara Martins <50105043+LeKinaSa@users.noreply.github.com>
2022-04-25 14:55:45 +0100

    Prolog: Using minimize(ObjFunc) instead of a loop

--- src/prolog/main.pl
+++ src/prolog/main.pl
@@ -7,44 +7,13 @@
 
 % job shop problem
 jobshop :-
-    % Data (Jobs, Tasks, Machines and Horizon)
+    reset_timer,
+
+    % Jobs and Horizon (Data)
     n_machines(Machines),
     get_jobs(Jobs)   , get_tasks(  Jobs ,   Tasks),
     length(Tasks , N), get_horizon(Tasks, Horizon),
 
-    % Reset Timer
-    reset_timer,
-
-    % Find a possible solution
-    jobshop_optimizer(Machines-Tasks-N, Horizon, 0).
-
-% jobshop_optimizer(+JobsData, +Horizon, +Iteration)
-%   job shop optimizer ; finds the best solution to the job shop problem
-jobshop_optimizer(JobsData, Horizon, Iteration) :-
-    % Print Iteration Number
-    nl, nl, write('         Iteration '), write(Iteration), nl,
-
-    % Search for the Current ObjFunc
-    jobshop_solver(JobsData, Horizon, ObjFunc),
-
-    % Print Iteration Time
-    print_time('         Iteration Time: '),
-
-    % Check if there is a Solution with a Smaller ObjFunc
-    NewHorizon is ObjFunc - 1,
-    NewIteration is Iteration + 1,
-    jobshop_optimizer(JobsData, NewHorizon, NewIteration).
-
-jobshop_optimizer(_, _, _) :-
-    % Print Iteration Time
-    print_time('         Iteration Time: '),
-
-    % Reached Optimal Solution
-    write('         Found Solution'), nl, nl, nl.
-
-% jobshop_solver(+JobsData, +Horizon, -ObjFunc) → JobsData = Machines-Tasks-TasksLength
-%   job shop solver ; find a solution to the job shop problem
-jobshop_solver(Machines-Tasks-N, Horizon, ObjFunc) :-
     % Decision Variables
     length(Chosen, N), domain(Chosen, 0, Machines),
     length(Start , N), domain(Start , 0,  Horizon),
@@ -59,7 +28,7 @@ jobshop_solver(Machines-Tasks-N, Horizon, ObjFunc) :-
     get_latest_finish( End, ObjFunc),
     append(Start  , End   , VarsAux),
     append(Chosen, VarsAux, Vars   ),
-    labeling([], Vars),
+    labeling([minimize(ObjFunc)], Vars),
 
     % Print
     print(Tasks, Start, End, Chosen, Horizon, ObjFunc).
