e9a372b6fc2721318bcb2d2ec1aa931783082101
Clara Martins <50105043+LeKinaSa@users.noreply.github.com>
2022-04-25 14:53:06 +0100

    Prolog: Main Loop for Minimization

--- src/prolog/main.pl
+++ src/prolog/main.pl
@@ -7,23 +7,25 @@
 
 % job shop problem
 jobshop :-
-    % Jobs and Horizon (Data)
-    get_jobs(Jobs),
-    get_max_timespan(Jobs, Horizon),
+    % Data (Jobs, Tasks, Machines and Horizon)
+    n_machines(Machines),
+    get_jobs(Jobs)   , get_tasks(  Jobs ,   Tasks),
+    length(Tasks , N), get_horizon(Tasks, Horizon),
 
     % Reset Timer
     reset_timer,
 
     % Find a possible solution
-    jobshop_optimizer(Jobs, Horizon, 0).
+    jobshop_optimizer(Machines-Tasks-N, Horizon, 0).
 
-% jobshop_optimizer(+Jobs, +Horizon, +Iteration) → job shop optimizer ; finds the best solution to the job shop problem
-jobshop_optimizer(Jobs, Horizon, Iteration) :-
+% jobshop_optimizer(+JobsData, +Horizon, +Iteration)
+%   job shop optimizer ; finds the best solution to the job shop problem
+jobshop_optimizer(JobsData, Horizon, Iteration) :-
     % Print Iteration Number
     nl, nl, write('         Iteration '), write(Iteration), nl,
 
     % Search for the Current ObjFunc
-    jobshop_solver(Jobs, Horizon, ObjFunc),
+    jobshop_solver(JobsData, Horizon, ObjFunc),
 
     % Print Iteration Time
     print_time('         Iteration Time: '),
@@ -31,7 +33,7 @@ jobshop_optimizer(Jobs, Horizon, Iteration) :-
     % Check if there is a Solution with a Smaller ObjFunc
     NewHorizon is ObjFunc - 1,
     NewIteration is Iteration + 1,
-    jobshop_optimizer(Jobs, NewHorizon, NewIteration).
+    jobshop_optimizer(JobsData, NewHorizon, NewIteration).
 
 jobshop_optimizer(_, _, _) :-
     % Print Iteration Time
@@ -40,27 +42,24 @@ jobshop_optimizer(_, _, _) :-
     % Reached Optimal Solution
     write('         Found Solution'), nl, nl, nl.
 
-% jobshop_solver(+Jobs, +Horizon, -ObjFunc) → job shop solver ; find a solution to the job shop problem
-jobshop_solver(Jobs, Horizon, ObjFunc) :-
-    % Tasks (Data)
-    get_tasks(Jobs, Tasks),
-    length(Tasks , N),
-
+% jobshop_solver(+JobsData, +Horizon, -ObjFunc) → JobsData = Machines-Tasks-TasksLength
+%   job shop solver ; find a solution to the job shop problem
+jobshop_solver(Machines-Tasks-N, Horizon, ObjFunc) :-
     % Decision Variables
-    length(Start , N), domain(Start , 0, Horizon),
-    length(End   , N), domain(End   , 0, Horizon),
-    length(Chosen, N), domain(Chosen, 0,    1   ),
+    length(Chosen, N), domain(Chosen, 0, Machines),
+    length(Start , N), domain(Start , 0,  Horizon),
+    length(End   , N), domain(End   , 0,  Horizon),
 
     % Constraints
-    task_duration(Tasks, Start, End),
-    only_one_chosen_alternative_task(Tasks, Chosen),
-    only_one_task_per_machine_at_a_time(Tasks, Start, End, Chosen),
-    
+    only_one_chosen_alternative_task(   Tasks, Chosen),
+    task_duration(                      Tasks, Chosen, Start, End),
+    only_one_task_per_machine_at_a_time(Tasks, Chosen, Start, End),
+
     % Solve
+    get_latest_finish( End, ObjFunc),
     append(Start  , End   , VarsAux),
-    append(VarsAux, Chosen, Vars   ),
+    append(Chosen, VarsAux, Vars   ),
     labeling([], Vars),
-    get_latest_finish(End, Chosen, ObjFunc),
 
     % Print
     print(Tasks, Start, End, Chosen, Horizon, ObjFunc).
