ca80d51451b9d992b0a7e8badcf2ebe940c78c50
Clara Martins <50105043+LeKinaSa@users.noreply.github.com>
2022-05-23 19:06:03 +0100

    OrTools: Remake (Without Output)

--- src/or-tools/constants.py
+++ src/or-tools/constants.py
@@ -5,6 +5,10 @@ INFINITE_MACHINE = 0
 TASK_MACHINE  = 0
 TASK_DURATION = 1
 
+MIN_START = 'min_start'
+MAX_END   = 'max_end'
+TASK      = 'task'
+
 class DataDifficulty(Enum):
     EXAMPLE = auto()
     EASY    = auto()
--- src/or-tools/data.py
+++ src/or-tools/data.py
@@ -1,13 +1,13 @@
 
 import json
 
-from constants import DataDifficulty
+from constants import DataDifficulty, TASK_DURATION, TASK
 
 EXAMPLE_DATA = 'data/example.json'
 EASY_DATA    = 'data/fab-easy.json'
 MEDIUM_DATA  = 'data/fab.json'
 
-def get_data(data_difficulty=DataDifficulty.EXAMPLE):
+def get_data(data_difficulty=DataDifficulty.MEDIUM):
     data_file = EXAMPLE_DATA
     if data_difficulty == DataDifficulty.EASY:
         data_file = EASY_DATA
@@ -18,7 +18,16 @@ def get_data(data_difficulty=DataDifficulty.EXAMPLE):
     
     with open(data_file, 'r') as file:
         jobs = json.load(file)
-    return jobs
+    return jobs, get_horizon(jobs)
+
+def get_horizon(jobs):
+    # Compute Horizon (worst case scenario)
+    horizon = 0
+    for job in jobs.values():
+        task = job[TASK]
+        max_task_duration = min([alt_task[TASK_DURATION] for alt_task in task])
+        horizon += max_task_duration
+    return horizon
 
 if __name__ == '__main__':
     # Get Data
