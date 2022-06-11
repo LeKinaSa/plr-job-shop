f11783e8e2c5085a2bd1ee4081274a047b8dd620
Clara Martins <clara.alves.martins@hotmail.com>
2022-06-11 19:56:39 +0100

    Or-tools & Docplex: Analyser

--- src/or-tools/analyse.py
+++ src/or-tools/analyse.py
@@ -1,14 +1,13 @@
-
-from ortools.sat.python.cp_model import CpSolver, OPTIMAL, FEASIBLE
-
 from os.path import exists
 from os import makedirs
 
 from main import jobshop
+from model import SolverType
 
 files = ['conflicts', 'branches', 'wall_time', 'status', 'obj_value']
 ORTOOLS_PATH = 'data/statistics/ortools/'
 PROLOG_PATH  = 'data/statistics/prolog/'
+DOCPLEX_PATH = 'data/statistics/docplex/'
 
 def analyser():
     for n_jobs in [2, 5, 10, 20, 40, 60, 80, 100, 125, 150]: # default: 75
@@ -39,7 +38,7 @@ def analyser():
         analyse(over_time_hours=over_time_hours)
     enter_files()
 
-    for time_out in [1, 5, 10, 15, 30, 45, 60, 120, 300]:
+    for time_out in range(1, 500):
         analyse(time_out=time_out)
 
 def analyse(n_jobs: int = 75, percent_alt_jobs: int = 50,
@@ -49,11 +48,14 @@ def analyse(n_jobs: int = 75, percent_alt_jobs: int = 50,
             time_out: int = 15):
 
     filename = f'{n_jobs}-{percent_alt_jobs}-{n_machines}-{percent_alt_machines}-{average_size_task}-{production_range}-{time_usage}-{over_time_hours}'
-    (solver, status) = jobshop(0, f'data/simulated/{filename}.json', time_out, False, True)
-    if status == OPTIMAL or status == FEASIBLE:
-        save_files(solver)
-    else:
-        print(f'Problem! File {filename}')
+    for solver_type in [0, 1]:
+        (solver, status) = jobshop(solver_type, f'data/simulated/{filename}.json', time_out, False, True)
+        s = SolverType(solver_type)
+        if status == s.OPTIMAL() or status == s.FEASIBLE():
+            save_files(solver, status, solver_type)
+        else:
+            s = 'OR-Tools' if solver_type == 0 else 'DOcplex'
+            print(f'Problem! File {filename} with solver {s}')
 
 def start_files(path: str) -> None:
     global files
@@ -64,14 +66,16 @@ def start_files(path: str) -> None:
 def enter_files() -> None:
     global files
     for filename in files:
-        with open(ORTOOLS_PATH + filename + '.txt', 'a') as file:
-            file.write('\n')
+        for path in [ORTOOLS_PATH, DOCPLEX_PATH]:
+            with open(path + filename + '.txt', 'a') as file:
+                file.write('\n')
 
-def save_files(solver: CpSolver) -> None:
+def save_files(solver: SolverType, status: int, solver_type: int) -> None:
     global files
-    solver_information = [solver.NumConflicts(), solver.NumBranches(), solver.WallTime(), solver.StatusName(), solver.ObjectiveValue()]
+    path = ORTOOLS_PATH if solver_type == 0 else DOCPLEX_PATH
+    solver_information = [solver.NumConflicts(), solver.NumBranches(), solver.WallTime(), solver.StatusName(status), solver.ObjectiveValue()]
     for (filename, info) in zip(files, solver_information):
-        with open(ORTOOLS_PATH + filename + '.txt', 'a') as file:
+        with open(path + filename + '.txt', 'a') as file:
             file.write(f', {info}')
 
 if __name__ == '__main__':
@@ -81,10 +85,14 @@ if __name__ == '__main__':
     else:
         if not exists(ORTOOLS_PATH):
             makedirs (ORTOOLS_PATH)
+        if not exists(DOCPLEX_PATH):
+            makedirs (DOCPLEX_PATH)
         start_files(ORTOOLS_PATH)
+        start_files(DOCPLEX_PATH)
+        
         if exists('data/simulated'):
             analyser()
             # analyse(5, 50, 4, 75, 50, 2, 80, 8)
         else:
             print('No files to analyse!')
-        # Total Time Used: 3226 seconds
+        # Total Time Used: ?? seconds
